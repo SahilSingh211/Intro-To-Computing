1a5fa14fd26fbe3971900adffd2387c17d92aa7c
Sahil <sahilsinghbhandari.bt21ele@pec.edu.in>
2022-01-26 13:31:17 +0530

    Output File For Assignment 2

--- Assignment2/Output.py
+++ Assignment2/Output.py
@@ -1,7 +1,7 @@
 Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
 
-== RESTART: C:\Users\Acer\OneDrive\Desktop\python\Assignment 2\Assignment 2.py =
+====== RESTART: C:\Users\Acer\OneDrive\Desktop\python\Assignment2\Input.py =====
 
 ===================================================== ASSIGNMENT 2, 21104050 =====================================================
 
@@ -21,7 +21,7 @@ New string is : Python is object oriented language
 
 Index of a is : 10
 
-The string is : Pythonisacasesensitivelanguage
+The string is : "Pythonisacasesensitivelanguage"
 
 
 ********************************************************************************
@@ -32,10 +32,10 @@ String Formatting
 Enter Your Name : Sahil Singh
 Enter Your SID : 21104050
 enter your department Name : EE
-Enter your CGPA : 9
-Hey, Sahil Singh here!
-My SID is: 21104050
-I am from EE and my CGPA is 9.0.
+Enter your CGPA : 8.9
+Hey, Sahil Singh Here!
+My SID is 21104050
+I am from EE department and my CGPA is 8.9.
 
 
 ********************************************************************************
@@ -60,16 +60,16 @@ a shifted right by 2 units and b by 4 units = 14 , 0
 The Greatest Number
 
 enter first number :12
-enter second number :45
-enter third number :34
-greatest number in given is : 45
+enter second number :23
+enter third number :43
+greatest number in given is : 43
 
 ********************************************************************************
 
       Program-5
 Finding word in String
 
-enter your sentence :I am fine, What is your name?
+enter your sentence :Hey!, I am Sahil. What is your name?
 yes
 
 
@@ -79,8 +79,8 @@ Program-6
 Triangle Checker from Side length
 
 Enter Length of First Side :12
-Enter Length of second Side :13
-Enter Length of Third Side :5
+Enter Length of second Side :5
+Enter Length of Third Side :13
 Yes.
 
 
